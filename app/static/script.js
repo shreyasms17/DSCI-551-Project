46239ac7ee18998175617d86c0ef18a2ade7d232
preetiusc <142448773+preetiusc@users.noreply.github.com>
2024-04-10 20:34:18 -0700

    products listing should not show products with invalid image url

--- app/static/script.js
+++
@@ -1,5 +0,0 @@
-function handleImageError(imageElement) {
-    // Find the parent row of the image and remove it from the table
-    var productRow = imageElement.closest('.product-row');
-    productRow.remove();
-}
