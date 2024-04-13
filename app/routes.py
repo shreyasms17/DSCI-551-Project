b3ebdc988b6a8987250f534995256be73fb5356c
Shreyas Mondikal Subramanya <shreyasms98@gmail.com>
2024-04-13 01:07:08 -0700

    added cart view and respective apis

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,16 @@
-from flask import render_template, request, redirect, url_for, abort
-from database import get_products_from_db, get_user_from_db, add_user_to_db, login_user
+from flask import render_template, request, redirect, url_for, abort, session
+from database import get_products_from_db, get_user_from_db, add_user_to_db, login_user, get_cart, save_cart
 
 # Assume the database paths are imported or defined here
 DATABASE_PATH_1 = 'data/databases/products_1.db'
 DATABASE_PATH_2 = 'data/databases/products_2.db'
 
 def init_app(app, login_manager):
-    @app.route('/')
+    @app.route("/")
+    def index():
+        return redirect(url_for("login"))
+    
+    @app.route('/listings')
     def product_listings():
         products_1 = get_products_from_db(DATABASE_PATH_1)
         products_2 = get_products_from_db(DATABASE_PATH_2)
@@ -22,15 +26,20 @@ def init_app(app, login_manager):
             # Check if the password entered is the 
             # same as the user's password
             print("User: " + str(user))
+            # print(dict(user[0]))
             if user and user[0]["password"] == request.form.get("password"):
                 # Use the login_user method to log in the user
                 login_user(user[0]["username"], DATABASE_PATH_1)
-                return redirect("/")
+                # retrieve cart info
+                session['cart'] = get_cart(user[0]['id'], DATABASE_PATH_1)
+                session['id'] = user[0]['id']
+                return redirect(url_for("product_listings"))
             else:
                 abort(404)
             # Redirect the user back to the home
             # (we'll create the home route in a moment)
-        return render_template("login.html")
+        else:
+            return render_template("login.html")
 
     @app.route('/signup', methods=["GET", "POST"])
     def register():
@@ -55,3 +64,35 @@ def init_app(app, login_manager):
             return user_1
         else:
             return get_user_from_db(user_id, DATABASE_PATH_2)
+    
+    @app.route('/add_to_cart/<product_name>', methods=['POST'])
+    def add_to_cart(product_name):
+        data = request.get_json()
+        price = data['price']
+        if 'cart' not in session:
+            session['cart'] = {}
+        session['cart'][product_name] = session['cart'].get(product_name, {"qty": 0, "price": price})
+        session['cart'][product_name]['qty'] += 1
+        session['cart'][product_name]['price'] = price
+        print(session)
+        session.modified = True
+        return redirect(url_for('product_listings'))
+    
+    @app.route('/cart')
+    def show_cart():
+        cart_items = session.get('cart', {})
+        # print(cart_items)
+        cart_details = []
+        total_price = 0
+        for product in cart_items:
+            cart_details.append({"name": product, "qty": cart_items[product]["qty"], "price": cart_items[product]["price"]})
+            total_price +=  cart_items[product]["qty"] * float(cart_items[product]["price"])
+        print(cart_details)
+        return render_template('cart.html', cart=cart_details, total_price=total_price)
+    
+    @app.route('/logout')
+    def logout():
+        # save cart info to db
+        save_cart(session['id'], session['cart'], DATABASE_PATH_1)
+        session.pop('cart', None)
+        return redirect(url_for('login'))
